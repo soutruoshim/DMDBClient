8bae7afb0217ca91e66fe213ce81f1e663bb0a39
soutruoshim <sout.rahim@gmail.com>
2023-03-26 22:34:05 +0700

    presentation artist

---
+++ app/src/main/java/com/srhdp/dmdbclient/presentation/artist/ArtistAdapter.kt
@@ -0,0 +1,46 @@
+package com.srhdp.dmdbclient.presentation.artist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.srhdp.dmdbclient.R
+import com.srhdp.dmdbclient.data.model.artist.Artist
+import com.srhdp.dmdbclient.databinding.ListItemBinding
+
+class ArtistAdapter():RecyclerView.Adapter<MyViewHolder>() {
+    private val artistList  = ArrayList<Artist>()
+    fun setList(artists:List<Artist>){
+        artistList.clear()
+        artistList.addAll(artists)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater:LayoutInflater = LayoutInflater.from(parent.context)
+        val binding:ListItemBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.list_item,
+            parent,
+            false
+        )
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+       holder.bind(artistList[position])
+    }
+
+    override fun getItemCount(): Int {
+       return artistList.size
+    }
+}
+class MyViewHolder(val binding:ListItemBinding):RecyclerView.ViewHolder(binding.root){
+    fun bind(artist: Artist){
+        binding.titleTextView.text = artist.name
+        binding.descriptionTextView.text = artist.popularity.toString()
+        val posterUrl:String ="https://image.tmdb.org/t/p/w500"+artist.profilePath
+        Glide.with(binding.imageView.context)
+            .load(posterUrl)
+            .into(binding.imageView)
+    }
+}
